e0607a2b9fe91886cc27618afa32fa2b01bc4a88
Siddhant8 <71453341+Siddhant8@users.noreply.github.com>
2021-06-15 16:37:58 -0700

    commit

--- src/main/java/com/example/spring/InfoController.java
+++ src/main/java/com/example/spring/InfoController.java
@@ -1,14 +1,9 @@
 package com.example.spring;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class InfoController {
@@ -19,28 +14,26 @@ public class InfoController {
         this.codeInterface = codeInterface;
     }
 
-    @GetMapping("/studyGuide/{id}")
-    public Optional<Info> getInfo(@PathVariable Long id) {
-
-        return codeInterface.findById(id)
-                
-    }
 
     @GetMapping("/studyGuide")
     public List<Info> everything() {
         return codeInterface.findAll();
     }
 
- 
-
     @PostMapping("/studyGuide")
     Info newInfo(@RequestBody Info newInfo) {
         return codeInterface.save(newInfo);
     }
-    
+
     @DeleteMapping("/studyGuide/{id}")
     public void deleteInfo(@PathVariable Long id){
-        return codeInterface.deleteById(id);
+
+        codeInterface.deleteById(id);
+    }
+
+    @GetMapping("/studyGuide/{id}")
+    public Optional<Info> getInfo(@PathVariable Long id){
+        return codeInterface.findById(id);
     }
 
 }
