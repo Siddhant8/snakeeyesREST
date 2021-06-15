d540e3ad1dc5f4629b327f2d0ed08eaa92259aec
Siddhant8 <71453341+Siddhant8@users.noreply.github.com>
2021-06-15 16:42:16 -0700

    commit

--- src/main/java/com/example/spring/InfoController.java
+++ src/main/java/com/example/spring/InfoController.java
@@ -15,23 +15,23 @@ public class InfoController {
     }
 
 
-    @GetMapping("/studyGuide")
+    @GetMapping("/study")
     public List<Info> everything() {
         return codeInterface.findAll();
     }
 
-    @PostMapping("/studyGuide")
+    @PostMapping("/study")
     Info newInfo(@RequestBody Info newInfo) {
         return codeInterface.save(newInfo);
     }
 
-    @DeleteMapping("/studyGuide/{id}")
+    @DeleteMapping("/study/{id}")
     public void deleteInfo(@PathVariable Long id){
 
         codeInterface.deleteById(id);
     }
 
-    @GetMapping("/studyGuide/{id}")
+    @GetMapping("/study/{id}")
     public Optional<Info> getInfo(@PathVariable Long id){
         return codeInterface.findById(id);
     }
